9c4ecfd3714c9c4cf294ba1e823c2b83eb4e95b7
Nguyễn Hữu Đức Vũ <nguyenducvuthht@gmail.com>
2023-08-10 23:51:50 +0700

    login register

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Regiter from './Pages/Regiter/Regiter';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/regiter" element={<Regiter/>}></Route>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
     </div>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,10 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
 import './Login.scss';
-import Logo from './whiteLogo.svg';
+import Logo from './logo.svg';
 
 const Login = () => {
 
@@ -12,75 +13,91 @@ const Login = () => {
 
     // check login
     const onFinish = (value) => {
-        localStorage.setItem("regiter", JSON.stringify(value));
+        localStorage.setItem("login", JSON.stringify(value));
 
         const { username, password } = value;
         console.log("Received values of form = ", value);
 
-        if (username === 'admin' && password === '12345678') {
+        if (username === JSON.parse(localStorage.getItem("regiter")).username && password === JSON.parse(localStorage.getItem("regiter")).password) {
+            swal("Login success!", "You clicked the button!", "success");
             navigate('/');
             localStorage.setItem("tokens", true);
+        } else {
+            swal({
+                title: "Thông tin đăng nhập không đúng?",
+                text: "Once deleted, you will not be able to recover this imaginary file!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              })
+              .then((willDelete) => {
+                if (willDelete) {
+                    swal(`Ussername: ${JSON.parse(localStorage.getItem("regiter")).username} - Mật khẩu: ${JSON.parse(localStorage.getItem("regiter")).password}`);
+                } else {
+                  swal("Vui lòng đăng nhập lại!");
+                }
+              });
         }
     }
 
     return (
         <div className='login'>
             <div className='login-main'>
-            <img src={Logo} alt="" />
-            <hr />
-            <h2>Login</h2>
-            <p>Your credentials</p>
-            <Form
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Username!',
-                        },
-                    ]}
+                <img src={Logo} alt="" />
+                <hr />
+                <h2>Login</h2>
+                <p className='login-desc'>Your credentials</p>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
                 >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Password!',
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>Remember me</Checkbox>
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox className='checkbox'>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="#!">
+                            Forgot password
+                        </a>
                     </Form.Item>
 
-                    <a className="login-form-forgot" href="">
-                        Forgot password
-                    </a>
-                </Form.Item>
-
-                <Form.Item>
-                    <Button type="primary" htmlType='submit' className="login-form-button">
-                        Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </Form.Item>
-            </Form>
+                    <Form.Item>
+                        <Button type="primary" htmlType='submit' className="login-form-button">
+                            Log in
+                        </Button>
+                        <p className='regiter-now'>Do not have an account ? <Link className='regiter-btn' to="/regiter">register now!</Link></p>
+                    </Form.Item>
+                </Form>
             </div>
         </div>
     );
