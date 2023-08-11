d78cd68bb1c70e888f9a72aeebf5d44684d07b21
unknown <pham.m.khang.98@gmail.com>
2023-08-11 12:57:07 +0700

    .

--- src/Components/TopTournaments/TopTournaments.jsx
+++ src/Components/TopTournaments/TopTournaments.jsx
@@ -15,6 +15,56 @@ const TopTournaments = () => {
             <Title className={cx('title')} title={'Top Tournament'} />
           </Col>
         </Row>
+        <Row className={cx('container')}>
+          <Col size={['l-3', 'm-6', 's-12']}>
+            <div className={cx('trophy')}>
+              <img
+                className={cx('img')}
+                src="https://parspng.com/wp-content/uploads/2022/05/Worldcupqatarpng.parspng.com-3.png"
+                alt=""
+              />
+              <div className={cx('trophy-name')}>
+                <h3>Europa League</h3>
+              </div>
+            </div>
+          </Col>
+          <Col size={['l-3', 'm-6', 's-12']}>
+            <div className={cx('trophy')}>
+              <img
+                className={cx('img')}
+                src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3508558/coppa-campioni-clipart-xl.png"
+                alt=""
+              />
+              <div className={cx('trophy-name')}>
+                <h3>Champion League</h3>
+              </div>
+            </div>
+          </Col>
+          <Col size={['l-3', 'm-6', 's-12']}>
+            <div className={cx('trophy')}>
+              <img
+                className={cx('img')}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Europa_League.svg/1200px-Europa_League.svg.png"
+                alt=""
+              />
+              <div className={cx('trophy-name')}>
+                <h3>Europa League</h3>
+              </div>
+            </div>
+          </Col>
+          <Col size={['l-3', 'm-6', 's-12']}>
+            <div className={cx('trophy')}>
+              <img
+                className={cx('img')}
+                src="https://i.pinimg.com/originals/f3/7a/21/f37a214bee82fb8097a872dec972d6f1.png"
+                alt=""
+              />
+              <div className={cx('trophy-name')}>
+                <h3>Premier League</h3>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Grid>
     </div>
   );
